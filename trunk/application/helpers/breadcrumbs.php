11e235b69825c131e2efd4ed06919e9f5919e543
jason.lewis1991 <jason.lewis1991@0133e754-e566-504c-61f0-c75c896f61b7>
2010-08-01 14:05:20 +0000

    See CHANGELOG for details

--- trunk/application/helpers/breadcrumbs.php
+++ trunk/application/helpers/breadcrumbs.php
@@ -1,11 +1,11 @@
 <?php
 
 	/**
-	 * breadcrumbs.helper.php
+	 * breadcrumbs.php
 	 *
 	 * Breadcrumbs Helper Class.
 	 */
-	class BreadcrumbsHelper extends Helpers {
+	class BreadcrumbsHelper {
 
 		/**
 		 * _text
@@ -18,8 +18,8 @@
 		 * @return string
 		 */
 		public function _text($hyperlinked = true, $default_home_text = 'Home', $separator = ' &raquo; '){
-			if($this->registry->is_library_loaded('Breadcrumbs')){
-				return $this->registry->Breadcrumbs->build_breadcrumb_text($default_home_text, $separator, $hyperlinked);
+			if(Spine::loaded('Breadcrumbs')){
+				return Breadcrumbs::build_breadcrumb_text($default_home_text, $separator, $hyperlinked);
 			}else{
 				return null;
 			}
@@ -35,8 +35,8 @@
 		 * @return array
 		 */
 		public function _array($hyperlinked = true, $default_home_text = 'Home'){
-			if($this->registry->is_library_loaded('Breadcrumbs')){
-				return $this->registry->Breadcrumbs->build_breadcrumb_array($default_home_text, $hyperlinked);
+			if(Spine::loaded('Breadcrumbs')){
+				return Breadcrumbs::build_breadcrumb_array($default_home_text, $hyperlinked);
 			}else{
 				return null;
 			}
