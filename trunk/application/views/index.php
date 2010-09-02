fc2b6b25ce8a422c2cfe87f75664c827c12ab4c6
jason.lewis1991 <jason.lewis1991@0133e754-e566-504c-61f0-c75c896f61b7>
2010-09-02 16:56:43 +0000

    Default view file updated to better reflect a properly configured Spine.

--- trunk/application/views/index.php
+++ trunk/application/views/index.php
@@ -1,18 +1,3 @@
-<?php echo $this->view('hello', true); ?>
-<?php echo $form->open(null, 'post', true, array('class' => 'form')); ?>
-<?php if($errors): ?>
-	Oh no! We found some errors when processing the form.
-	<ul>
-		<?php foreach($errors as $msg): ?>
-		<li><?php echo $msg; ?></li>
-		<?php endforeach; ?>
-	</ul>
-<?php endif; ?>
-<?php echo $form->fieldset_open('Login'); ?>
-<?php echo $form->label('Username', 'username'); ?>: <?php echo $form->input('username', $post->source('username'), array('id' => 'username')); ?><br />
-<?php echo $form->label('Password', 'pasword'); ?>: <?php echo $form->password('password', null, array('id' => 'password')); ?><br />
-<?php echo $form->submit('submit', 'Login'); ?>
-<?php
-	echo $form->fieldset_close();
-	echo $form->close();
-?>
+<h2>SpinePHP</h2>
+Hey! Welcome, are you reading this message?<br />
+That means you have configured Spine correctly. Make sure to get online and seek out help if you need it.
